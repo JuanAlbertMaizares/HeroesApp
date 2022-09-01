b2298836a5378c0e18830a2e00b763053f5db167
Maizares, Juan <maizaresjuan.coder@gmail.com>
2022-08-31 21:04:34 -0300

    rutas y pages

--- src/heroes/components/HeroCard.jsx
+++ src/heroes/components/HeroCard.jsx
@@ -1,6 +1,13 @@
+import { Link } from 'react-router-dom';
 
-
-import React from 'react'
+//creamos un mini compo que solo se usa aqui, no se exporta.
+const CharactersByHero = ({characters, alter_ego}) => {
+    // if (alter_ego === characters ) return (<></>);
+    // return <p>{characters}</p>
+    return (alter_ego === characters)
+    ? <></>
+    : <p>{characters}</p>;
+}
 
 export const HeroCard = ({
     id,
@@ -12,19 +19,21 @@ export const HeroCard = ({
 }) => {
     const heroImageUrl = `/assets/heroes/${id}.jpg`;
     return (
-        <div className="col">
+        <div className="col animate__animated animate__fadeIn">
             <div className="card">
                 <div className="row no-gutters">
                     <div className="col-4">
-                        <img src={heroImageUrl} alt="{superhero}" className="card-img" />
+                        <img src={heroImageUrl} alt={superhero} className="card-img" />
                     </div>
                     <div className="col-8">
                     <div className="card-body">
                             <h5 className='card-title'>{superhero}</h5>
                             <p className='card-text'>{alter_ego}</p>
-                            {
-                                (alter_ego !== characters) && (<p>{characters}</p>)
-                            }
+                            <CharactersByHero characters={characters} alter_ego={alter_ego} />
+                            <p className="card-text">
+                                <small className="text-muted">{first_appearance}</small>
+                            </p>
+                            <Link to={`/hero/${id}`}>Mas...</Link>
                     </div>
                     </div>
                 </div>

c841f03a9d67def4523222440bddc641185c46c9
Maizares, Juan <maizaresjuan.coder@gmail.com>
2022-08-31 23:02:08 -0300

    carga final de datos

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -1,6 +1,65 @@
+import { useLocation, useNavigate } from 'react-router-dom';
+import queryString from 'query-string';
+import { useForm } from '../../hooks/useForm';
+import {HeroCard} from '../components';
+import { getHeroesByName } from '../helpers';
+
 
 export const SearchPage = () => {
+  const navigate = useNavigate();   
+  const location = useLocation();
+  const {q = ''} = queryString.parse( location.search );
+  const heroes = getHeroesByName(q);
+  const showSearch = (q.length === 0);
+  const showError = (q.length > 0) && heroes.length === 0;
+  const { searchText, onInputChange } = useForm({
+    searchText: q
+  });
+  const onSearchSubmit = (event) => {
+      event.preventDefault();
+      if (searchText.trim().length <= 1) return; 
+      navigate(`?q=${ searchText }`);
+    }
   return (
-    <h1>SearchPage</h1>
+    <>
+        <h2>Buscar</h2>
+        <hr />
+        <div className="row">
+            <div className="col-5">
+              <h4>Buscando</h4>
+              <hr />
+              <form onSubmit={onSearchSubmit}>
+                  <input type="text" name="searchText" placeholder="Buscar un heroe" 
+                  className="form-control" 
+                  autoComplete="off" 
+                  value={searchText}
+                  onChange={onInputChange}
+                  />
+                  <button className="btn btn-outline-primary mt-2">
+                    Buscar
+                  </button>
+              </form>
+            </div>
+            <div className="col-7">
+                <h4>Resultado</h4>
+                <hr />
+
+                <div 
+                  className="alert alert-primary animate__animated animate__fadeIn"
+                  style={{display: showSearch ? '' : 'none'}}
+                >Busca un heroe</div>
+                <div 
+                  className="alert alert-danger animate__animated animate__fadeIn"
+                  style={{display: showError ? '' : 'none'}}
+                >No se encontro el heroe <b>{q}</b> </div>
+
+                {
+                  heroes.map(hero => (
+                    <HeroCard key={hero.id} {...hero} />
+                  ))
+                }
+            </div>
+        </div>
+    </>
   )
 }
--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -34,6 +34,13 @@ export const Navbar = () => {
                     >
                         DC
                     </NavLink>
+
+                    <NavLink 
+                        className={({isActive})=>`nav-item nav-link ${isActive? 'active': ''}`}
+                        to="/search"
+                    >
+                        Buscar
+                    </NavLink>
                 </div>
             </div>
 

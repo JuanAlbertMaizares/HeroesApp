ee41ceb4e27ec688ff514bcefb4a55e6683ae51f
Maizares, Juan <maizaresjuan.coder@gmail.com>
2022-09-10 10:59:45 -0300

    fix route private

--- src/router/PrivateRoute.jsx
+++ src/router/PrivateRoute.jsx
@@ -2,12 +2,17 @@ import { useContext } from "react"
 import { Navigate, useLocation } from "react-router-dom";
 import { AuthContext } from "../auth/context/AuthContext"
 
+// compo HOC que recibe compoHijo, componentes
 export const PrivateRoute = ({children}) => {
+    //usa info de un contexto
     const {logged} = useContext(AuthContext);
+    //se usa un hook Location para info de la direccion donde esta parado.
     const {pathname, search } = useLocation();
 
     const lastPath = pathname + search;
     localStorage.setItem('lastPath', lastPath); 
+
+    // elige que hacer con el compo hijo, que 
     return (logged)
         ? children
         : <Navigate to="/login" />
